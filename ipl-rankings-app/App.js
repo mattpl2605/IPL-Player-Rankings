c1c4bfa65eea2e22a98a206a11c130516d95a55e
Matthew Port Louis <88897685+bilbaothanos6@users.noreply.github.com>
2023-12-21 15:46:48 -0600

    basic homescreen ui setup

--- ipl-rankings-app/App.js
+++ ipl-rankings-app/App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function App() {
+  const [season, setSeason] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [items, setItems] = useState([
+    {label: '2023', value: '2023'},
+    // Add other IPL seasons here
+    // {label: 'Year', value: 'Year'}
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Select an IPL Season</Text>
+      <DropDownPicker
+        open={open}
+        value={season}
+        items={items}
+        setOpen={setOpen}
+        setValue={setSeason}
+        setItems={setItems}
+        zIndex={3000}
+        zIndexInverse={1000}
+      />
+      {season && (
+        <View style={styles.buttonContainer}>
+          <Button title="Batter Rankings" onPress={() => {/* Implement action */}} />
+          <Button title="Bowler Rankings" onPress={() => {/* Implement action */}} />
+        </View>
+      )}
     </View>
   );
 }
@@ -13,8 +38,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    marginTop: 20,
+    width: '100%',
   },
 });
